68be5b6eee8cbe842abda0eae12de9e35e4b53a8
determined6730@gmail.com <determined6730@gmail.com>
2018-06-25 08:37:19 +0000

    update vimrc

--- setup.sh
+++ setup.sh
@@ -2,12 +2,12 @@
 
 sudo apt-get update
 sudo apt-get upgrade -yy
-
+sudo apt-get install -yy vim
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 sudo apt-get install -yy ctags 
 sudo apt-get install -yy cscope 
 
-mv ./vimrc ~/.vimrc 
-sudo mv ./mkcscope.sh /usr/bin/mkcscope.sh 
+cp ./vimrc ~/.vimrc 
+sudo cp ./mkcscope.sh /usr/bin/mkcscope.sh 
 sudo chmod +x /usr/bin/mkcscope.sh
 
